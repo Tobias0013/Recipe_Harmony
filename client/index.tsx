d465a78bbdb6c72c15d18fdaec201320b24ec60b
Tobias Vinblad <tobias.vinblad0013@stud.hkr.se>
2024-04-30 12:06:48 +0200

    installed react-router-dom

--- client/index.tsx
+++ client/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import "./style.css";
 
 import Header from "./component/header/header";
+import Home from "./pages/home/home";
+import Test from "./pages/test";
 
 const rootElem = document.getElementById("root");
 
@@ -10,10 +13,14 @@ if (!rootElem) {
     process.exit(1);
 }
 
-const root = ReactDOM.createRoot(rootElem);
 
+const root = ReactDOM.createRoot(rootElem);
+<Header loggedIn={false} location={"Home"} />;
 root.render(
-    <>
-        <Header loggedIn={false} location={"Home"} />
-    </>
+    <BrowserRouter>
+        <Routes>
+            <Route index element={<Home />} />
+            <Route path="/test" element={<Test />} />
+        </Routes>
+    </BrowserRouter>
 );
