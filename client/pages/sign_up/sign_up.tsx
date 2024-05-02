f75e2d2ebbc192b5b5d08e6e98b9c711be641b4c
KristofferLarsson5 <KristofferLarsson5>
2024-05-02 20:04:15 +0200

    Sign up design started

---
+++ client/pages/sign_up/sign_up.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUpForm from "../../component/sign_up_form/sign_up"; 
+
+const SignUpPage: React.FC = () => {
+    return (
+        <div>
+            <SignUpForm />
+        </div>
+    );
+};
+
+export default SignUpPage;
