bcda59067fa9014dd7034b17d72fafbf581fb1d6
Anton Bryhagen <anton.bryhagen@gmail.com>
2024-05-02 14:49:04 +0200

    Add session creation function with basic password validation

--- server/controller/database/userDB.ts
+++ server/controller/database/userDB.ts
@@ -29,6 +29,32 @@ async function addUser(full_name: string, password: string, email: string){
     }
 }
 
+
+/*
+    Verify user credentials and return JWT token for further authorization
+    TODO:
+    Implement JWT Token
+*/
+async function createUserSession(email: string, password: string){
+    try{
+        const res = await User.find({email: email});
+        if(res.length === 0){
+            //no user with that email
+            return{error: 401}
+        }
+        const hashedPassword: string = res[0].password;
+        const validPassword: boolean = await bcrypt.compare(password, hashedPassword);
+        
+        if(!validPassword){
+            //incorrect password
+            return{error: 401}
+        }
+
+    }catch(e){
+        return{error: e}
+    }
+}
+
 export default {
     user: {
         add: addUser
