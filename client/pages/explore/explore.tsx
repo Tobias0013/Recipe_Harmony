4c8a58b024bd22242c0b395332b81cf90ca027f5
Tobias0013 <tobias.vinblad@hotmail.com>
2024-05-04 14:47:53 +0200

    implemented redirects to Explore search

--- client/pages/explore/explore.tsx
+++ client/pages/explore/explore.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./explore.css";
-import { useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 import Header from "../../component/header/header";
 import Footer from "../../component/footer/footer";
@@ -14,11 +14,26 @@ export default function Explore() {
 
     const exploreSearch = [
         //TODO redo based on recipes
-        "Less than 10 min",
-        "Less than 20 min",
-        "Less than 30 min",
-        "some tag",
-        "some tag",
+        {
+            text: "Less than 10 min",
+            url: "http://localhost:8080/explore?cookTimeLess=10",
+        },
+        {
+            text: "Less than 20 min",
+            url: "http://localhost:8080/explore?cookTimeLess=20",
+        },
+        {
+            text: "Less than 30 min",
+            url: "http://localhost:8080/explore?cookTimeLess=30",
+        },
+        {
+            text: "Pasta",
+            url: "http://localhost:8080/explore?ingreedient=pasta",
+        },
+        {
+            text: "Chicken",
+            url: "http://localhost:8080/explore?ingreedient=rice",
+        },
     ];
 
     const fetchRecipes = async () => {
@@ -36,11 +51,12 @@ export default function Explore() {
                 <main>
                     <section className="explore-section">
                         {queryParameters.size <= 0 &&
-                            exploreSearch.map((search, index) => (
+                            exploreSearch.map((s, index) => (
                                 <ExploreSearch
                                     key={index}
                                     id={index}
-                                    text={search}
+                                    text={s.text}
+                                    url={s.url}
                                 />
                             ))}
                     </section>
@@ -64,15 +80,17 @@ export default function Explore() {
     );
 }
 
-function ExploreSearch(prop: { id: number; text: string }) {
-    const { id, text } = prop;
+function ExploreSearch(prop: { id: number; text: string; url: string }) {
+    const { id, text, url } = prop;
 
     return (
-        <div className={`explore-search s${id}`}>
-            <div>
-                <p>{text}</p>
+        <Link to={url}>
+            <div className={`explore-search s${id}`}>
+                <div>
+                    <p>{text}</p>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }
 
