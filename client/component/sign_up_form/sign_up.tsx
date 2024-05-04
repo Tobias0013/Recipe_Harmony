661097b5b2bc8fd26de620190b1ed2aae4df625e
Anton Bryhagen <anton.bryhagen@gmail.com>
2024-05-04 20:33:04 +0200

    Add form submission functionality to sign-up form

--- client/component/sign_up_form/sign_up.tsx
+++ client/component/sign_up_form/sign_up.tsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import "./sign_up.css"; 
 
 const sign_up_form: React.FC = () => {
+    const [fullName, setFullName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        try{
+            const response = await fetch("http://localhost:3000/api/users", {
+                method: "POST",
+                headers: {
+                    "Content-Type" : "application/json"
+                },
+                body: JSON.stringify({
+                    full_name: fullName,
+                    password: password,
+                    email: email
+                })
+            });
+
+            console.log(response);
+        
+        }catch(err){
+            console.log(err);
+        }
+    }
+
     return (
         <div className="signup-form">
-            <form>
-                <input type="text" placeholder="Full Name" className="input-field" />
-                <input type="text" placeholder="Email" className="input-field" />
-                <input type="password" placeholder="Password" className="input-field" />
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Full Name" className="input-field" onChange={(e) => setFullName(e.target.value)}/>
+                <input type="text" placeholder="Email" className="input-field" onChange={(e) => setEmail(e.target.value)}/>
+                <input type="password" placeholder="Password" className="input-field" onChange={(e) => setPassword(e.target.value)}/>
                 <button type="submit" className="submit-button">Sign Up</button>
             </form>
             <p>Already have an account? <a href="/login">Login here</a></p>
