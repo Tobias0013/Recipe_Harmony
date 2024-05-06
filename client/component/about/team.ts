00718ae38664f8ece044a2d81832483efb0887b7
Kristoffer <kristoffer.larsson01@gmail.com>
2024-05-06 10:10:52 +0200

    Beginning of the about page, design, info about the project and team started

---
+++ client/component/about/team.ts
@@ -0,0 +1,13 @@
+export interface TeamMember {
+    name: string;
+    role: string;
+    bio: string;
+}
+
+export const team: TeamMember[] = [
+    { name: "Tobias Vinblad", role: "Head of marketing", bio: "John is passionate about building innovative solutions that solve real-world problems." },
+    { name: "Anton Bryhagen", role: "Head of sales", bio: "Johhn brings a wealth of experience in digital marketing and brand strategy." },
+    { name: "Filip Danielsson", role: "Head of IT", bio: "John brings a wealth of experience in digital marketing and brand strategy." },
+    { name: "Alexander Pålsson", role: "Head of Tech", bio: "John brings a wealth of experience in digital marketing and brand strategy." },
+    { name: "Kristoffer Larsson", role: "Worker", bio: "John brings a wealth of experience in digital marketing and brand strategy." }
+];
