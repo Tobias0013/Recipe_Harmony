e42d41cfddf341a3a6a072be68ef806340049aab
Tobias Vinblad <tobias.vinblad0013@stud.hkr.se>
2024-05-06 12:51:40 +0200

    fix import householdDB

--- server/routes/household.ts
+++ server/routes/household.ts
@@ -2,7 +2,7 @@ import express, { Router, Request, Response } from "express";
 import { isValidObjectId } from "mongoose";
 
 import HouseholdModel from "../mongoose/household";
-import householdDB from '.../controller/database/householdDB';
+import householdDB from '../controller/database/householdDB';
 
 const HouseholdRouter: Router = Router();
 
