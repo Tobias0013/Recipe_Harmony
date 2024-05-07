7c12895e7d650e7a62208a67d7c057c6923fa427
Anton Bryhagen <anton.bryhagen@gmail.com>
2024-05-07 23:42:15 +0200

    Implement use of access denied component to block pages if logged in

--- client/pages/login/login.tsx
+++ client/pages/login/login.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import LoginForm from "../../component/login_form/login"; 
+import AccessDenied from "../../component/access_denied/access_denied";
 
 const LoginPage: React.FC = () => {
-    return (
-        <div>
-            <LoginForm />
-        </div>
-    );
+    const jwtExists = sessionStorage.getItem("jwt");
+    if(jwtExists){
+        return (
+            <AccessDenied 
+                redirectTo = "/"
+                reason = "You Are Already Logged In"
+            />
+        )
+    }else{
+        return (
+            <div>
+                <LoginForm />
+            </div>
+        );
+    }
+    
 };
 
 export default LoginPage;
