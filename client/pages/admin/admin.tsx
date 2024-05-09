b8ca8bbfc26a4c3bd695cab3a57cd1c40d58e195
Anton Bryhagen <anton.bryhagen@gmail.com>
2024-05-09 21:04:12 +0200

    Add protected fetch to provide admin page protection

--- client/pages/admin/admin.tsx
+++ client/pages/admin/admin.tsx
@@ -2,10 +2,34 @@ import React from "react";
 import Users from "../../component/admin/users";
 import Receips from "../../component/admin/recipes";
 import Households from "../../component/admin/households";
+import userAPI from "../../controller/fetch/users";
+import { useEffect, useState } from "react";
 
 const AdminPage: React.FC = () => {
+    const jwtExists = sessionStorage.getItem("jwt");
+
+    const [users, setUsers] = useState<any>();
+
+    useEffect(() => {
+        const getData = async () => {
+            if (jwtExists) {
+                try {
+                    const response: Response = await userAPI.user.getAll(jwtExists)
+                    const data = await response.json();
+                    if(!data.error){
+                        setUsers(data)
+                    }
+                } catch (e) {
+                    console.log(e)
+                }
+            }
+        }
+        getData();
+
+    }, [])
+
     return (
-        <div>
+        users && (<div>
             <h1>ADMIN PAGE</h1>
             <div>
                 <Users />
@@ -13,7 +37,9 @@ const AdminPage: React.FC = () => {
             <Receips />
             <Households />
         </div>
+        )
     );
+
 };
 
 export default AdminPage;
