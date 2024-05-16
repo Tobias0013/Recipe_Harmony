0b4e082571510a52e21919b5e60d8ad7958d6734
Tobias0013 <tobias.vinblad@hotmail.com>
2024-05-16 22:05:25 +0200

    Refactor admin page to redirect if not admin

--- client/pages/admin/admin.tsx
+++ client/pages/admin/admin.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
+import { useNavigate } from "react-router-dom";
+
 import User from "../../component/admin/user";
 import Receips from "../../component/admin/recipes";
 import Households from "../../component/admin/households";
 import userAPI from "../../controller/fetch/users";
-import { useEffect, useState } from "react";
 
 const AdminPage: React.FC = () => {
     const jwtExists = sessionStorage.getItem("jwt");
+    const navigate = useNavigate();
 
     const [users, setUsers] = useState<any>();
     const [userError, setUserError] = useState<any>();
@@ -15,20 +17,21 @@ const AdminPage: React.FC = () => {
         Move this function to each admin component instead of this page
     */
     useEffect(() => {
-        (async () => {
-            if (jwtExists) {
-                try {
-                    const response: Response = await userAPI.user.getAll(
-                        jwtExists
-                    );
-                    const data = await response.json();
-                    if (!data.error) {
-                        setUsers(data);
-                    }
-                } catch (e) {
-                    console.log(e);
-                    setUserError(e);
+        if (!jwtExists) {
+            navigate("/");
+            return;
+        }
+
+        const fetchUsers = (async () => {
+            try {
+                const response: Response = await userAPI.user.getAll(jwtExists);
+                const data = await response.json();
+                if (!data.error) {
+                    setUsers(data);
                 }
+            } catch (e) {
+                console.log(e);
+                setUserError(e);
             }
         })();
     }, []);
@@ -36,7 +39,7 @@ const AdminPage: React.FC = () => {
     return (
         users && (
             <div>
-                <h1 style={{ fontSize: "3rem" }} >ADMIN PAGE</h1>
+                <h1 style={{ fontSize: "3rem" }}>ADMIN PAGE</h1>
                 <section>
                     <h1 style={{ fontSize: "2rem" }}>USER DETAILS</h1>
 
