8ab17b3c529f41be0de72cffc0930a86a382a722
Tobias0013 <tobias.vinblad@hotmail.com>
2024-05-16 23:04:12 +0200

    Refactor admin page to fetch and display recipes with Recipe component

---
+++ client/component/admin/recipe.tsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import "./recipe.css";
+
+interface Recipe {
+    id: string;
+    name: string;
+    image: string;
+    cookTimeMinutes: number;
+    rating: number;
+}
+
+const Recipe = (props: { recipe: any }) => {
+    const { recipe } = props;
+
+    const handleRemoveRecipe = (id: string) => {
+        // Implement logic to remove recipe with the specified id
+    };
+
+    const handleEditRecipe = (id: string) => {
+        // Implement logic to edit recipe with the specified id
+    };
+
+    return (
+        <div className="recipes">
+            <div className="recipe">
+                <div className="recipe-actions">
+                    <button
+                        className="remove-button"
+                        onClick={() => handleRemoveRecipe(recipe.id)}
+                    >
+                        X
+                    </button>
+                    <button onClick={() => handleEditRecipe(recipe.id)}>
+                        Edit
+                    </button>
+                </div>
+                <img
+                    src={
+                        recipe.image.url
+                            ? recipe.image.url
+                            : recipe.image.base64
+                    }
+                    alt={recipe.name}
+                />
+                <div className="recipe-details">
+                    <h2>{recipe.name}</h2>
+                    <p>Cook Time: {recipe.cookTimeMinutes} minutes</p>
+                    <p>Rating: {recipe.rating}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Recipe;
--- client/pages/admin/admin.tsx
+++ client/pages/admin/admin.tsx
@@ -1,17 +1,22 @@
-import React, { useEffect, useState }  from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import User from "../../component/admin/user";
-import Receips from "../../component/admin/recipes";
+import Recipe from "../../component/admin/recipe";
 import Households from "../../component/admin/households";
 import userAPI from "../../controller/fetch/users";
+import recipeAPI from "../../controller/fetch/recipes";
 
 const AdminPage: React.FC = () => {
     const jwtExists = sessionStorage.getItem("jwt");
     const navigate = useNavigate();
 
     const [users, setUsers] = useState<any>();
-    const [userError, setUserError] = useState<any>();
+    const [errorUser, setErrorUser] = useState<any>();
+    const [recipes, setRecipes] = useState<any>();
+    const [errorRecipes, setErrorRecipes] = useState<any>();
+    const [households, setHouseholds] = useState<any>();
+    const [errorHouseholds, setErrorHouseholds] = useState<any>();
     /*
         TODO:
         Move this function to each admin component instead of this page
@@ -26,12 +31,30 @@ const AdminPage: React.FC = () => {
             try {
                 const response: Response = await userAPI.user.getAll(jwtExists);
                 const data = await response.json();
-                if (!data.error) {
-                    setUsers(data);
+
+                if (data.error) {
+                    return setErrorUser((prev) => prev + `${data.error}\n`);
                 }
+                setUsers(data);
             } catch (e) {
                 console.log(e);
-                setUserError(e);
+                setErrorUser((prev) => prev + "Internal error\n");
+            }
+        })();
+
+        const fetchRecipes = (async () => {
+            try {
+                const { error, recipes } = await recipeAPI.get({
+                    limit: 9999,
+                });
+
+                if (error) {
+                    return setErrorRecipes((prev) => prev + `${error}\n`);
+                }
+                setRecipes(recipes);
+            } catch (e) {
+                console.log(e);
+                setErrorRecipes((prev) => prev + "Internal error\n");
             }
         })();
     }, []);
@@ -43,15 +66,25 @@ const AdminPage: React.FC = () => {
                 <section>
                     <h1 style={{ fontSize: "2rem" }}>USER DETAILS</h1>
 
-                    {userError ? (
-                        <div>Error: {userError}</div>
+                    {errorUser ? (
+                        <div>Error: {errorUser}</div>
                     ) : (
                         users.map((user) => {
                             return <User key={user._id} user={user} />;
                         })
                     )}
                 </section>
-                <Receips />
+                <section>
+                    <h1>RECIPES</h1>
+                    {errorRecipes || !recipes ? (
+                        <div>Error: {errorRecipes}</div>
+                    ) : (
+                        recipes.map((recipe) => {
+                            return <Recipe key={recipe._id} recipe={recipe} />;
+                        })
+                    )}
+                </section>
+
                 <Households />
             </div>
         )
