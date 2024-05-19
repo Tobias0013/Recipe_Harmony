f55f3aa58e92bc8afca171243b840079a719e2f7
Anton Bryhagen <anton.bryhagen@gmail.com>
2024-05-19 15:18:56 +0200

    Implement feature to get houshold data for household page

--- client/component/household/household.tsx
+++ client/component/household/household.tsx
@@ -1,29 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useState from "react-usestateref"
 import { Link } from 'react-router-dom';
 import './household.css';
+import householdAPI from "../../controller/fetch/household";
 
 function Household() {
-    const sampleHousehold = {
-        _id: "663a27521f8b65134101798b",
-        name: "Kristoffers home",
-        members: ["Kristoffer Larsson"],
-        shopping_list: ["Margherita Pizza"],
-        ingredients: ["Flour", "Parsley"]
-    };
+
+    interface IHousehold {
+        _id: string,
+        name: string,
+        members: string[],
+        shopping_list: any,
+        ingredients: any
+    }
+
+    const [household, setHousehold, householdRef] = useState<IHousehold>({
+        _id: "",
+        name: "",
+        members: [],
+        shopping_list: {},
+        ingredients: {}
+    });
+
+    useEffect(() => {
+        async function getHousehold(){
+            const householdById = await householdAPI.getById();
+            setHousehold(householdById.household);
+            console.log(householdRef.current)
+        }
+        
+        getHousehold();
+    }, [])
+
     return (  
         <div className="household-page">
             <div className="header-container">
-                <h1 className="header">Household: {sampleHousehold.name}</h1>
+                <h1 className="header">{household.name}</h1>
             </div>
             <p className="household-id">
             <span className="id-label">ID:</span>
-            <span className="id-value">{sampleHousehold._id}</span>
+            <span className="id-value">{household._id}</span>
             </p>
             <section className="section">
                 <div className="members">
                     <h2>Members</h2>
                     <ul className="list">
-                        {sampleHousehold.members.map((member, index) => (
+                        {household.members.map((member, index) => (
                             <li key={index}>{member}</li>
                         ))}
                     </ul>
