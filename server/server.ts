5b01546ffcb24cb4d028a3ddebfce70a8b472be2
Tobias Vinblad <tobias.vinblad0013@stud.hkr.se>
2024-05-21 13:21:54 +0200

    fix: refresh page error. By serving index.html on non api call

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 import express, { Router, Request, Response} from "express";
 import cors from "cors";
+import fs from 'fs';
 
 import apiRouter from "./routes";
 import { port, uri } from "./controller/config";
@@ -19,6 +20,17 @@ app.use("/api", apiRouter);
 app.get("/status", (req: Request, res: Response) =>{
     res.json("Server is upp and running!")
 });
+
+app.use(async (req, res, next) => {
+    res.set("Content-Type", "text/html; charset=UTF-8")
+    fs.readFile(__dirname.replace("server", "") + "dist/index.html", (err, data) =>{
+        if (err) {
+            res.status(500).send("Something went wrong while reading file");
+            return;
+        }
+        res.send(data);
+    });
+})
 
 app.listen(port, () => {
     console.log(`Server started successfully.\nListening on http://localhost:${port}`);
